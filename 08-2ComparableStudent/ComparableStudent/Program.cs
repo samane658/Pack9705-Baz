5c418bad2fe0a85ac0b8b34ba7b3c7ac37bd0a80
samane658 <42388358+samane658@users.noreply.github.com>
2018-10-09 22:31:27 +0330

    comparableStudent

---
+++ 08-2ComparableStudent/ComparableStudent/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComparableStudent
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            student[] students = new student[]
+               {
+                    new student() { Name = "Ross", Family = "Geller", Average = 80 },
+                    new student() { Name = "Chandler", Family = "Bing", Average = 70 },
+                    new student() { Name = "Monica", Family = "Geller", Average = 90 },
+                    new student() { Name = "aaa", Family = "aaaaa", Average = 50 },
+                    new student() { Name = "aaa", Family = "zzzzz", Average = 90 }
+               };
+
+            Array.Sort(students);
+            foreach (student student in students)
+            {
+                Console.WriteLine(student);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 08-2ComparableStudent/ComparableStudent/student.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComparableStudent
+{
+    class student :IComparable
+    {
+        public string Name { get; set; }
+        public string Family { get; set; }
+        public double Average { get; set; }
+
+        public int CompareTo(object obj)
+        {
+            student std = (student)obj;
+            if (this.Family.CompareTo(std.Family) == 0)
+            {
+                if (this.Name.CompareTo(std.Name) == 0)
+                    return 0;
+                else
+                    if (this.Name.CompareTo(std.Name) == 1)
+                    return 1;
+                else
+                    return -1;
+            }
+            else
+                if (this.Family.CompareTo(std.Family) == 1)
+                return 1;
+            else
+                return -1;
+
+        }
+
+        public override string ToString()
+        {
+            return $"{Name} {Family} [{Average}]";
+        }
+    }
+}
